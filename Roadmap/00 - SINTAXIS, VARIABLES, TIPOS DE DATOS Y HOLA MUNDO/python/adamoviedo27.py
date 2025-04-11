d6e2d60ebaa868037c5546edb374e780299fcfae
Adamoviedo27 <adamoviedoheredia@gmail.com>
2025-04-10 20:29:25 -0400

    Create adamoviedo27.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/adamoviedo27.py
@@ -0,0 +1,23 @@
+# https://www.python.org/
+
+
+#Esto es un comentario usual en PYTHON
+'''
+Esto es un comentario
+de varias lineas en python
+'''
+
+#Variables y Constantes
+
+motivoParaAprenderAprogramar =  "superacion"
+CONSTANTE = "Python no tiene constantes"
+
+
+#Tipos de datos
+DatoINT = 10
+DatoFloat = 1.1
+DatoStr = "Lol"
+LosLakersGanaranElAnillo = True
+CadenaDeTexto = ["No", "he", "tomado", "cafe", "hoy"]
+
+print("Hola, Python")
